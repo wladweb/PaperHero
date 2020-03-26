9e38f83b06be51d1f4181f2bd01d15c49cc6b71e
wladweb <wladwebwork@gmail.com>
2020-03-26 14:15:14 +0300

    level decor

---
+++ Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _offset = transform.position - Player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + _offset;
+    }
+}
