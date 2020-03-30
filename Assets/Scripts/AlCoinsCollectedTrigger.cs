43b558a576444c6fcb73018f534564941a960a1d
wladweb <wladwebwork@gmail.com>
2020-03-30 20:07:40 +0300

    coin collecting mechanic

---
+++ Assets/Scripts/AlCoinsCollectedTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlCoinsCollectedTrigger : MonoBehaviour
+{
+    private Coin[] _coins;
+
+    private void OnEnable()
+    {
+        _coins = gameObject.GetComponentsInChildren<Coin>();
+
+        foreach (Coin coin in _coins)
+        {
+            coin.Collected += OnAllCoinsCollected;
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (Coin coin in _coins)
+        {
+            coin.Collected -= OnAllCoinsCollected;
+        }
+    }
+
+    private void OnAllCoinsCollected()
+    {
+        Debug.Log("All coins collected!");
+    }
+}
---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _collected = new UnityEvent();
+
+    public event UnityAction Collected 
+    {
+        add => _collected.AddListener(value);
+        remove => _collected.RemoveListener(value);
+    }
+
+    public void CollectHandler()
+    {
+        Debug.Log("Collected");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _collected.Invoke();
+        Destroy(gameObject);
+    }
+}
+
