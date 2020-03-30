2e5d8a9cdfc4ac43c1a5f90c18eec8cdf260daff
wladweb <wladwebwork@gmail.com>
2020-03-30 19:00:32 +0300

    be done

---
+++ Assets/Scripts/BeeBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeBehaviour : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void TakeDamageHandler()
+    {
+        _animator.SetTrigger("Damage");
+    }
+
+    public void DeathHandler()
+    {
+        _animator.SetTrigger("Death");
+        Destroy(gameObject, 2f);
+;    }
+}
